71e31165dfcab3e47af05973ed2139d7f0fd7733
Lloyd Colart <colart.lloyd.pro@gmail.com>
2022-10-26 11:15:54 +0200

    Test Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,11 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello World</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
